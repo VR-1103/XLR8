7c7db1b356fe3b2c01b00ca8f1b88e271689f330
Ishaan Manhar <210070033@iitb.ac.in>
2022-08-20 00:30:25 +0530

    Motor Analog Trial

--- motordriver/motorcontrol_analog_trial.ino
+++ motordriver/motorcontrol_analog_trial.ino
@@ -101,3 +101,27 @@ void loop() {
         break;
     }
 }
+int cmd()
+{
+  // put your main code here, to run repeatedly:
+//  Serial.print("X: ");
+//  Serial.print(analogRead(34));
+//  Serial.print(" Y: ");
+//  Serial.print(analogRead(35));
+//  Serial.println();
+  if ((abs(analogRead(34)-1740)<100) and (abs(analogRead(35)-1740)<100)){
+      return 0;
+    }
+  if ((abs(analogRead(34)-1740)<100) and (abs(analogRead(35))<100)){
+      return 1;
+    }
+  if ((abs(analogRead(34)-1740)<100) and (abs(analogRead(35)-4095)<100)){
+      return 2;
+    }
+  if ((abs(analogRead(34))<100) and (abs(analogRead(35)-1740)<100)){
+      return 3;
+    }
+  if ((abs(analogRead(34)-4095)<100) and (abs(analogRead(35)-1740)<100)){
+     return 4;
+    }
+}
