7e267f2c52e3ac55506deb5c4090d4b6ddb9d50e
Maalavika <csmaalavika27@gmail.com>
2022-08-22 23:23:59 +0530

    motor slider

--- motordriver/motor_driver_slider.ino
+++ motordriver/motor_driver_slider.ino
@@ -20,7 +20,7 @@ const int IN2 = 19;
 const int ENB = 25;
 const int IN3 = 26;
 const int IN4 = 27;
-int spd=200;
+int spd;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -38,8 +38,90 @@ void setup() {
    * After this we can start adding some code to control the ESP32.
    * We can make requests to the server through a webpage and make it perform different tasks.
    */
-  server.on ( "/", HTTP_handleRoot );
-  server.onNotFound ( HTTP_handleRoot );
+
+  server.on("/F", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    
+    DFORWARD();
+    request->send_P(200, "text/plain", "");   
+  });
+
+  server.on("/B", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we recieve a "turn-off" request from the webpage
+    
+    DBACKWARD();
+    request->send_P(200, "text/plain", "");
+  });
+
+  server.on("/R", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we recieve a "turn-off" request from the webpage
+    
+    DLEFT(); //Opposite for now coz thats what works correctly
+    request->send_P(200, "text/plain", "");
+  });
+
+  server.on("/L", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we recieve a "turn-off" request from the webpage
+    
+    DRIGHT();//Opposite for now coz thats what works correctly
+    request->send_P(200, "text/plain", "");
+  });
+  server.on("/S", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    STOP();
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/1", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(1, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/2", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(2, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/3", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(3, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/4", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(4, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/5", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(5, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/6", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(6, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/7", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(7, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/8", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(8, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/9", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(9, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+  server.on("/10", HTTP_GET, [](AsyncWebServerRequest *request){
+    // the following code runs whenever we receive a "turn-on" request from the webpage
+    spd = map(10, 1, 10, 100, 250);
+    request->send_P(200, "text/plain", "");   
+  });
+
   server.begin(); 
 }
 
@@ -97,23 +179,5 @@ void STOP(){
 }
 
 // the loop function runs over and over again forever
-void loop() {        // starts the web server
-  server.handleClient();
-  command = server.arg("State");
-  if (command=="F") DFORWARD();
-  else if (command == "B") DBACKWARD();
-  else if (command == "L") DRIGHT(); //L makes DRIGHT happen because of some error
-  else if (command == "R") DLEFT(); //Similar to above
-  else if (command == "0") STOP();
-
-  if (command<11 && command>0) spd=map(command, 1, 10, 100, 250);
-  
-}
-void HTTP_handleRoot(void) {
-
-if(server.hasArg("State") ){
-       Serial.println(server.arg("State"));
-  }
-  server.send ( 200, "text/html", "" );
-  delay(1);
-}
+void loop() {                      // starts the web server
+}             
